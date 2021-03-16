c2c45633dfba982cf50f8c0ebc1ed44247286536
Damish <Damishnisal100@gmail.com>
2021-03-16 12:43:54 +0530

    Added Feedback PHP

---
+++ feedback.php
@@ -0,0 +1,12 @@
+<?php
+require  "conn.php";
+
+
+
+$type = $_POST['type'];
+$userName = $_POST['userName'];
+$feedbackMassage = $_POST['feedbackMassage'];
+$time = $_POST['time'];
+$requestID = $_POST['requestID'];
+
+// echo ("hello");
