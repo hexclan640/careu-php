a2ab9ad891d112f951e1da7e28e37541a123f77c
Damish <Damishnisal100@gmail.com>
2021-03-28 10:20:49 +0530

    updated it

--- updateProfile.php
+++ updateProfile.php
@@ -7,14 +7,13 @@ $lastName = $_POST['lastName'];
 $email = $_POST['email'];
 $phoneNumber = $_POST['phoneNumber'];
 
-
 $mysql_qry_relation = "select userId from servicerequester where username like '$username' and status like '1'";
 $result = mysqli_query($conn, $mysql_qry_relation);
 $row = $result->fetch_assoc();
 $userId = (int) $row['userId'];
 
 
-$mysql_query = "UPDATE servicerequester SET firstName='$firstName', lastName='$lastName',email='$email',phoneNumber='$phoneNumber' WHERE userId = $userId";
+$mysql_query = "UPDATE servicerequester SET firstName='$firstName, lastName='$lastName',email='$email',phoneNumber='$phoneNumber' WHERE userId = $userId";
 
 if (mysqli_query($conn, $mysql_query)) {
     echo "Record updated";
